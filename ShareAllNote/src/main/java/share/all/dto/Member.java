876e63c990de47b3d995904e2f423029d3d7ab30
DESKTOP-1R0GHKB\hanna <hanna10299@gmail.com>
2018-03-24 18:21:46 +0900

    에러난거 다시다시

--- ShareAllNote/src/main/java/share/all/dto/Member.java
+++ ShareAllNote/src/main/java/share/all/dto/Member.java
@@ -11,6 +11,54 @@ public class Member {
 	private String role; // 권한
 	private String friend_id; // 친구아이디 타입 알아서
 	private int g_num; // 가입한 그룹(타입알아서)
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProfile_img() {
+		return profile_img;
+	}
+	public void setProfile_img(String profile_img) {
+		this.profile_img = profile_img;
+	}
+	public int getOnoff() {
+		return onoff;
+	}
+	public void setOnoff(int onoff) {
+		this.onoff = onoff;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getFriend_id() {
+		return friend_id;
+	}
+	public void setFriend_id(String friend_id) {
+		this.friend_id = friend_id;
+	}
+	public int getG_num() {
+		return g_num;
+	}
+	public void setG_num(int g_num) {
+		this.g_num = g_num;
+	}
 	
 	
 	
